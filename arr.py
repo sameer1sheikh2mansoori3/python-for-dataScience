cbddf8813224d7d9ecc019df8b3ccaa5bec6f308
sameer1sheikh2mansoori3 <94507751+sameer1sheikh2mansoori3@users.noreply.github.com>
2024-08-26 14:15:12 +0530

    ignite our app

---
+++ arr.py
@@ -0,0 +1,13 @@
+import numpy as np
+# lets build a 2d array
+lst1 = [1,2,3,4]
+lst2 = [11,22,33,44]
+lst3 = [111,222,333,444]
+myArr = np.array([lst1 , lst2 , lst3])
+# assignment solution
+print(myArr[: , 0::3])
+print(myArr<2)
+# reshape can be also done
+
+# arrange can also be done
+
